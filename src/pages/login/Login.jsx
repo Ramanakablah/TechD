a1e4c1067f65124a49085f1fc1451e4dad5f4b1a
Raman <alexesramon0909@gmail.com>
2022-03-18 13:40:46 +0530

    depo3

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 import Footer from '../../components/footer/Footer';
 import './Login.css';
 
@@ -6,37 +6,45 @@ export default function SignIn() {
     return (
         <>
 
-        <div className='container'>
-            <div className='box_1'>
-            <img className="img" src="assets/logo(log in).png" alt="logo" />
-            <div className='box_11'>
-            <h2 className="heading1">Welcome to TechDirective </h2>
-            <input className="emailcss" type="email" placeholder = "&#xf0e0;  Email"/><br />
-            <input className="passwordcss" type="password" placeholder="&#xf084;  Password" /><br />
-            <span>Forget Password?</span>
+            <div className='container'>
+                <div className='box_1'>
+                    <img className="img" src="assets/logo(log in).png" alt="logo" />
+                    <div className='box_11'>
+                        <h2 className="heading1">Welcome to TechDirective </h2>
+                        <input className="emailcss" type="email" placeholder="&#xf0e0;  Email" /><br />
+                        <input className="passwordcss" type="password" placeholder="&#xf084;  Password" /><br />
+                        <span>Forget Password?</span>
 
-            <div className='box_12'>
-            <button className="btnsignup">Sign In</button><br />
-            <p className="or">or</p>
-            <img className="img2 img11" src="assets/google.jpg" alt="google"/>
-            <img className="img2" src="assets/facebook.png" alt="facebook"/>
-            <img className="img2" src="assets/twitter.png"alt="twitter" />
+                        <div className='box_12'>
+                            <button className="btnsignup">Sign In</button><br />
+                            <p className="or">or</p>
+                            <a href="/">
+                                <img className="img2 img11" src="assets/google.jpg" alt="google" />
+                            </a>
+                            <a href="https://www.facebook.com/techdirective/">
+                                <img className="img2" src="assets/facebook.png" alt="facebook" />
+                            </a>
+                            <a href="https://twitter.com/chandravanshi_1">
+                                <img className="img2" src="assets/twitter.png" alt="twitter" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='box_2'>
+                    <div className='box_21'>
+                        <h2 className="rheading">Hello!</h2>
+                        <p className="textr">Create your account and start your journey with us.</p>
+                        <Link to="sign-up">
+                            <button className="btnsignup">Sign Up</button>
+                        </Link>
+                    </div>
+                </div>
+
+
+
             </div>
-            </div>
-     </div>
-            
-        <div className='box_2'>
-            <div className='box_21'>
-            <h2 className="rheading">Hello!</h2>
-            <p className="textr">Create your account and start your journey with us.</p>
-            <button className="btnsignup">Sign Up</button>
-            </div>
-        </div>
-          
-      
-           
-        </div>
-         <Footer />
+            <Footer />
         </>
     )
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom"
 import Topbar from "../../components/Navbar/Navbar";
 import Footer from '../../components/footer/Footer';
 import './register.css'
@@ -6,30 +6,33 @@ export default function Login() {
     return (
         <div className="register">
             <div className="cont">
-
-
                 <div className="leftcont">
                     <img className="img" src="assets/logo(sign in).png" alt="logo" />
                     <h2 className="sideheading">Welcome Back!</h2>
                     <p className="textt">Stay connected with us, for more updates.</p>
-                    <button className="signinbtn">Sign In</button>
+                    <Link to="/login">
+                        <button className="signinbtn">Sign In</button>
+                    </Link>
                 </div>
-
 
                 <div className="rightcont">
                     <h2 className="heading">Create Account</h2>
-
                     <input className="name fs" type="text" placeholder="&#xf007;  Name" /><br />
                     <input className="email fs" type="email" placeholder="&#xf0e0;  Email" /><br />
                     <input className="password fs" type="password" placeholder="&#xf084;  Password" /><br />
 
                     <button className="signupbtn">Sign Up</button><br />
                     <p className="or">or</p>
-                    <img className="img1 img11" src="assets/google.jpg" />
-                    <img className="img1" src="assets/facebook.png" />
-                    <img className="img1" src="assets/twitter.png" />
+                    <a href="/">
+                        <img className="img1 img11" src="assets/google.jpg" />
+                    </a>
+                    <a href="https://www.facebook.com/techdirective/">
+                        <img className="img1" src="assets/facebook.png" />
+                    </a>
+                    <a href="https://twitter.com/chandravanshi_1">
+                        <img className="img1" src="assets/twitter.png" />
+                    </a>
                 </div>
-
             </div>
 
             <Footer />
